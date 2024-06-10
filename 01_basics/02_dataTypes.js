88fc973498d5c5c54f2606a0d1aea51d41d60fbc
Priyadarshi Sen <54526527+PriyadarshiSen@users.noreply.github.com>
2024-06-10 16:10:20 +0000

    Advance datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -20,3 +20,35 @@ let isLoggeIn = false
 // object
 
 console.log(typeof null);
+
+// ************ Advance **************** //
+// Primitive datatypes
+// 7 types : String, number, boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol("123")
+const anotherID = Symbol("123")
+
+console.log(id === anotherID);
+
+const bigNumber = 34329842837923829484281n
+
+// NonPrimitive
+// Reference Type : array, objects, functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "Sunny",
+    age: 22,
+}
+
+const myFunction = function () {
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
