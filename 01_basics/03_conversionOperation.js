b5142370f503319d505069aabece85a1b1412b43
Priyadarshi Sen <54526527+PriyadarshiSen@users.noreply.github.com>
2024-06-10 05:10:14 +0000

    Conversion of Data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+
+// let valueNumber = Number(score)
+
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true =>; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
